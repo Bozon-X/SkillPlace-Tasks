f07d9194cede001391cd50734d0cc1d2f5bb4339
Bozon-X <kabachokins@gmail.com>
2024-12-10 17:12:47 +0300

    09/12

---
+++ main.py
@@ -0,0 +1,37 @@
+# 1st program
+
+print(9 ** 0.5 * 5)
+
+
+
+# 2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+
+
+# 3rd program
+
+task_1 = (2 * 2 + 2)
+
+print(task_1)
+
+task_2 = (2 * (2 + 2))
+
+print(task_2)
+
+print(task_1 == task_2)
+
+
+
+# 4th program
+
+string = "123.456"
+
+print(int(string[4])) # моё решение
+
+
+
+string_1 = float("123.456")
+
+print (int(string_1 * 10 % 10)) # решение по алгоритму
